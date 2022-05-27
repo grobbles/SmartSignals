1f33a09d8c5125e7f57a678521e662823a6fe023
Uwe Roder <uweroder@gmail.com>
2022-05-27 20:53:30 +0200

    adjust for beat deployment

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ requirements = []
 with open("requirements.txt", "r") as requirements_file:
     requirements.append(requirements_file.read())
 
-classifiers = [
     # How mature is this project? Common values are
     #   1 - Planning
     #   2 - Development
     #   3 - Alpha
     #   4 - Beta
     #   5 - Production/Stable
-    'Development Status :: 3 - Alpha',
-    "Intended Audience :: Developers",
-    "Topic :: Communications",
-    "Topic :: Utilities"
     # 'License :: OSI Approved :: MIT License',
-    "Programming Language :: Python",
+    # 'Development Status :: 3 - Alpha',
+    # "Intended Audience :: Developers",
+    # "Topic :: Communications",
+    # "Topic :: Utilities"
+    # "Programming Language :: Python",
+classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
